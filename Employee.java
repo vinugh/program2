fa7dc2f698be2fb8f97e56dcf5e3cf9cc23fa182
vinugh <01fe23bca166@kletech.ac.in>
2025-05-06 15:10:14 +0530

    save file

---
+++ Employee.java
@@ -0,0 +1,21 @@
+package Employee;
+public class Employee {
+    private String name;
+    private int id;
+    private double salary;
+
+    public Employee(String name, int id, double salary) {
+        this.name = name;
+        this.id = id;
+        this.salary = salary;
+    }
+    public void dispay() {
+        System.out.println("Employee Name: " + name);
+        System.out.println("Employee ID: " + id);
+        System.out.println("Salary: " + salary);
+    }
+    public static void main(String[] args) {
+        Employee emp = new Employee("John Doe", 12345, 50000.0);
+        emp.dispay();
+    }
+}
